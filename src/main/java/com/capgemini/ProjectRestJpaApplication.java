334df5bd4c451bd7d49c7c35ac3e85a1d3883070
ganesh <guttikondaganesh26@gmail.com>
2021-05-17 10:07:08 +0530

    v1

---
+++ src/main/java/com/capgemini/ProjectRestJpaApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRestJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectRestJpaApplication.class, args);
+	}
+
+}
